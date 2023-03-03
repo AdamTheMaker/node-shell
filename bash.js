34b54281c3e6cc54dd898c4dbed24c8f2f9d3748
AdamTheMaker <adam.bojoh@gmail.com>
2023-03-03 16:55:47 -0500

    ls module added

--- bash.js
+++ bash.js
@@ -1,9 +1,11 @@
 process.stdout.write("prompt > ");
 const pwd = require("./pwd");
+const ls = require("./ls");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  pwd(cmd)
+  pwd(cmd);
+  ls(cmd);
 
   process.stdout.write("You typed: " + cmd);
   process.stdout.write("\nprompt > ");
